e6a81f912dee8e00efd82b5eb8686df4506692b1
Your NameOllebolle44 <olle.alfred.klasson@gmail.com>
2022-03-02 16:33:47 +0100

    Demo commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env pybricks-micropython
+
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.A)
+
+right_sensor = ColorSensor(Port.S1)
+left_sensor = ColorSensor(Port.S2)
+# Write your program here.  
+ev3.speaker.beep()
+worth = left_sensor.reflection()
+print(worth)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=65.5, axle_track=104)
+
+# Go forward and backwards for one meter.
+#robot.straight(1000)
+ev3.speaker.beep()
+
+#robot.straight(-1000)
+ev3.speaker.beep()
+
+# Turn clockwise by 360 degrees and back again.
+#robot.turn(360)
+ev3.speaker.beep()
+
+#robot.turn(-360)
+ev3.speaker.beep()  
