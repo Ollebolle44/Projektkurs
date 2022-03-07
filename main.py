8f10267efc5fa39fae08abe3aa226a734160995d
Your NameOllebolle44 <olle.alfred.klasson@gmail.com>
2022-03-07 17:33:03 +0100

    fixat syntaxfelen nu så den kör

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ right_motor = Motor(Port.A)
 right_sensor = ColorSensor(Port.S1)
 left_sensor = ColorSensor(Port.S2)
 
-both_sensor = right_sensor + left_sensor 
-
 #Gör så att båda motorerna kan gå samtidigt
 robot = DriveBase(left_motor, right_motor, wheel_diameter=65.5, axle_track=104)
 
@@ -38,14 +36,19 @@ while True:
     #2. Höger Sensor ser svart svänger höger
     #3. Vänster Sensor ser svart svänger vänster
     #4. Allt är fine kör rakt
+
+    rsensor = right_sensor.reflection()
+    lsensor = left_sensor.reflection()
+    both_sensor = rsensor + lsensor
+    
     if both_sensor > Both_White:
         turn_rate = 35
-    elif right_sensor < Black:
+    elif rsensor < Black:
         turn_rate = 20
-    elif left_sensor < Black:
+    elif lsensor < Black:
         turn_rate = -20
     else:
         turn_rate = 0
     
-    robot.drive(DRIVE_SPEED, turn_rate)
-    wait(10)
+    robot.drive(Drive_Speed, turn_rate)
+    
