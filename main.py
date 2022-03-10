938c0a9b70f92020c2246add9e300d291c0355bc
Your NameOllebolle44 <olle.alfred.klasson@gmail.com>
2022-03-10 18:58:46 +0100

    Börjat fiffla med korsning och lämning av bollar

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -19,11 +19,17 @@ right_motor = Motor(Port.A)
 #Färgsensorer
 right_sensor = ColorSensor(Port.S1)
 left_sensor = ColorSensor(Port.S2)
+color_sensor = ColorSensor(Port.S3)
+#Börjar med ett eftersom den skippar den första där uppplockaren kommer stå
+deposit = 1
+pickup = 0
+turn_crossection = 3
 
 #Gör så att båda motorerna kan gå samtidigt
 robot = DriveBase(left_motor, right_motor, wheel_diameter=65.5, axle_track=104)
 
-#Värden på färgena
+#Värden på färgena + sätter in färger
+POSSIBLE_COLORS = [Color.RED, Color.GREEN, Color.BLUE, Color.WHITE]
 Black = 10
 Both_White = 50
 
@@ -35,13 +41,48 @@ while True:
     #uppdatering av sensorernas värde samt summa av dem
     rsensor = right_sensor.reflection()
     lsensor = left_sensor.reflection()
+    color = color_sensor.color()
     both_sensor = rsensor + lsensor
-    print(lsensor)
-    #1. Failsafe
-    #2. Höger Sensor ser svart svänger höger
-    #3. Vänster Sensor ser svart svänger vänster
-    #4. Allt är fine kör rakt
-    if both_sensor > Both_White:
+    
+    #While loopen kommer sättas igång när alla bollar lämnats och används för att plocka upp nya
+    
+    #1. Den åkt förbi 4 färger och kommer lämna av en bollsort beroende på den sista färgen
+    #2. Den åker förbi en av de fyra färgena och svänger till höger(behöver en grej så den vet när den ska svänga)
+    #3. Den åker förbi en gul färg och kommer addera en variabel elr ngt för att veta när den ska svänga
+    #4. Failsafe
+    #5. Höger Sensor ser svart svänger höger
+    #6. Vänster Sensor ser svart svänger vänster
+    #7. Allt är fine kör rakt
+    
+    while pickup == 3:
+        #linjeföljekod med samma en engångs höger sväng kod och sen en meetup kod med upplockaren
+        #trycksensor när de är vid varanda -> vänta antal sekunder -> sätt pickup =0 och kör på gamla
+        #programmet
+        pickup = 0
+
+    if color in POSSIBLE_COLORS and deposit==4:
+        deposit = 0
+        #kör tills trycksensor trycks åt?
+        #sedan gör den ngn sekvens så den kommer rätt med gatesen
+        #-> till nästa ifsats för att släppa bollar
+        #if satser med 3 olika färgena som öppnar rätt gate beroende på färg
+        if color == COLOR.BLUE:
+            #blå öppna gate 1
+        elif color == COLOR.GREEN:
+            #grön öppna gate 2
+        elif color == COLOR.RED:
+            #röd öppna gate 3
+        pickup +=1
+        #här ksk vi ska ha en variabel som adderas med 1 och när den varit på alla 3
+        #så åker den tillbaka till upplockaren?
+    elif color in POSSIBLE_COLORS and turn_crossection == 4:
+        #300 mm 90 grader 3 sekunder
+        robot.drive_time(300, 90, 3000)
+        deposit +=1
+        turn_crossection = 0
+    elif color == COLOR.YELLOW:
+        turn_crossection += 1
+    elif both_sensor > Both_White:
         turn_rate = 180
     elif rsensor < Black:
         turn_rate = 150
