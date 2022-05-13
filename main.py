dd1553b1b36b72d270ba2b6112a23868d008073d
Your NameOllebolle44 <olle.alfred.klasson@gmail.com>
2022-05-13 16:11:54 +0200

    Innan kod

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ ev3 = EV3Brick()
 #Motorer
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.A)
-#gate_big = Motor(Port.C)
-#gate_medsmall = Motor(Port.D)
+gate_big = Motor(Port.C)
+gate_medsmall = Motor(Port.D)
 
 #Färgsensorer
 right_sensor = ColorSensor(Port.S2)
@@ -27,7 +27,7 @@ left_sensor = ColorSensor(Port.S3)
 color_sensor = ColorSensor(Port.S1)
 #Trycksensor
 #touch_sensor = TouchSensor(Port.S4)
-#ultrasonic_sensor = UltrasonicSensor(Port.S4)
+ultrasonic_sensor = UltrasonicSensor(Port.S4)
 
 #Bluetooth
 #server = BluetoothMailboxServer()
@@ -60,7 +60,7 @@ pickup = 0
 sv = 0
 turn = "left"
 beenyellow = False
-
+idle = False
 #Oändlig loop
 while True:
     rsensor = right_sensor.reflection()
@@ -78,8 +78,7 @@ while True:
         rsensor = right_sensor.reflection()
         lsensor = left_sensor.reflection()
         color = color_sensor.color()
-        #ultrasensor = ultrasonic_sensor.distance()
-        ultrasensor = 500
+        ultrasensor = ultrasonic_sensor.distance()
         #print(ultrasensor)
         #print("höger:")
         #print(rsensor)
@@ -89,13 +88,13 @@ while True:
             #sekvens vid upphämtning av bollar
             Drive_Speed = 0
             turn_rate = 0
-            robot.straight(50)
-            wait(4000)
-            robot.straight(-100)
-            wait(1000)
-            robot.turn(350)
-            wait(1000)
+            robot.drive_time(200, 0, 3000)
+            wait(3000)
+            robot.drive_time(-300, 0, 2000)
             
+            #failsafe ksk
+            robot.drive_time(180, 0, 1100)
+            robot.turn(-160)
             #reset values
             pickup=0
             Drive_Speed = 40
@@ -104,7 +103,8 @@ while True:
             deposit = 1
             pickup = 0
             beenyellow = False
-        elif color in POSSIBLE_COLORS and turn_crossection >= 4:
+            idle = False
+        elif color in POSSIBLE_COLORS and turn_crossection >= 4 and idle == False:
             #Sekvens för sväng kan behövas finjusteras men den funkar atm
             #test ^^
             robot.drive_time(35, 0, 800)
@@ -121,7 +121,6 @@ while True:
                 turnkonst += 5
                 robot.turn(-5*k)
                 lastcolor = color_sensor.color()
-                wait(500)
                 if turnkonst == 80:
                     robot.turn(80*k)
                     k *= -1
@@ -131,12 +130,14 @@ while True:
             robot.turn(-15*k)
 
             #Svängsekvens
-            robot.drive_time(180, 0, 1100)
-            robot.turn(180)
+            robot.drive_time(200, 0, 1100)
+            robot.turn(-160)
+            robot.drive_time(100, 0, 1200)
+            robot.turn(350)
             
 
             #Failsafe
-            k = 1
+            k = -1
             turnkonst = 0
             sv = 0
             lastcolor = color_sensor.color()
@@ -147,19 +148,19 @@ while True:
                 turnkonst += 5
                 robot.turn(-5*k)
                 lastcolor = color_sensor.color()
-                wait(500)
                 if turnkonst == 80:
                     robot.turn(80*k)
                     k *= -1
                     turnkonst = 0
                     sv += 1
 
-            
-            Drive_Speed = 40
+            idle = True
+            Drive_Speed = 0
+            turn_rate = 0
             turn_crossection = 0
             print("sväng:")
             print(color)
-        elif color == Color.YELLOW:
+        elif color == Color.YELLOW and idle == False:
             turn_crossection += 1
             #Sekvens så att färgsensr åker förbi tejpen och inte läser av den igen
             robot.drive_time(32, 0, 800)
@@ -176,19 +177,18 @@ while True:
                 turnkonst += 5
                 robot.turn(-5*k)
                 lastcolor = color_sensor.color()
-                wait(500)
                 if turnkonst == 80:
                     robot.turn(80*k)
                     k *= -1
                     turnkonst = 0
                     sv += 1
-        elif rsensor < Black:
+        elif rsensor < Black and idle == False:
             #turn_rate = 50
             print("höger")
             
             turn_rate = ((Black-rsensor)*5 + 40)
             print(turn_rate)
-        elif lsensor < Black:
+        elif lsensor < Black and idle == False:
             #turn_rate = -50
             print("vänster")
             
@@ -275,7 +275,29 @@ while True:
         #Sätter turn_crossection till 3 för att den ska svänga nästa gång den läser av en gul färg
         turn_crossection = 3
 
+        robot.drive_time(32, 0, 800)
         #Failsafe
+        k=-1
+        turnkonst = 0
+        sv = 0
+        lastcolor = color_sensor.color()
+        while lastcolor != Color.BLACK:
+            if sv >=2:
+                robot.straight(20)
+                sv=0
+            turnkonst += 6
+            robot.turn(-6*k)
+            lastcolor = color_sensor.color()
+            #wait(500)
+            if turnkonst == 78:
+                robot.turn(78*k)
+                k *= -1
+                turnkonst = 0
+                sv += 1
+        
+        #Sekvens där roboten åker fram 320 mm för att inte missa avlastningen av bollarna
+        robot.straight(50)
+        #failsafe
         if turn == "right":
             k = -1
         elif turn == "left":
@@ -287,23 +309,21 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv=0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 80:
-                robot.turn(80*k)
+            #wait(500)
+            if turnkonst == 78:
+                robot.turn(78*k)
                 k *= -1
                 turnkonst = 0
                 sv += 1
-        robot.turn(-15*k)
-        #Sekvens där roboten åker fram 320 mm för att inte missa avlastningen av bollarna
-        robot.straight(320)
+        robot.straight(270)
         wait(2000)
 
         print("avlastning:")
         print(dropcolor20)
-        """
+        
         if beenyellow == True:
             #if satser med 3 olika färgena som öppnar rätt gate beroende på färg
             if dropcolor == Color.BLUE:
@@ -321,7 +341,7 @@ while True:
                 wait(1000)
                 gate_big.run_target(90, 0)
                 print("Blå")
-            elif dropcolor == Color.RED:
+            elif dropcolor == Color.GREEN:
                 #färg 2 öppna gate 2 mellersta
                 gate_medsmall.run_target(60, -60)
                 wait(1000)
@@ -333,9 +353,9 @@ while True:
                 robot.straight(-30)
                 wait(1000)
                 gate_medsmall.run_target(60, 0)
-                print("röd")
+                print("grön")
             elif dropcolor == Color.WHITE:
-                #färg 3 öppna gate 3 största bollarna
+                #färg 3 öppna minsta bollarna
                 gate_medsmall.run_target(60, 60)
                 wait(1000)
                 #wiggle wiggle
@@ -349,7 +369,7 @@ while True:
                 gate_medsmall.run_target(60, 0)
                 print("vit")
         
-        """
+        
         print("avlastning efter:")
         print(dropcolor20)
         #Adderar pickup med 1 varje deposit(pickup=3 -> whileloop som åker och plockar upp nya bollar)
@@ -374,12 +394,12 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv=0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 90:
-                robot.turn(90*k)
+            #wait(500)
+            if turnkonst == 120:
+                robot.turn(120*k)
                 k *= -1
                 turnkonst = 0
                 sv += 1
@@ -388,7 +408,7 @@ while True:
         #Framtiden kan en failsafe i form av en whileloop skapas för att hitta den svarta färgen
     elif color in POSSIBLE_COLORS and deposit ==3:
         deposit+=1
-        robot.drive_time(32, 0, 800)
+        #robot.drive_time(32, 0, 800)
     elif color in POSSIBLE_COLORS and turn_crossection >= 4:
         #Sekvens för sväng kan behövas finjusteras men den funkar atm
         #test ^^
@@ -406,25 +426,23 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv = 0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 80:
-                robot.turn(80*k)
+            #wait(500)
+            if turnkonst == 84:
+                robot.turn(84*k)
                 k *= -1
                 turnkonst = 0
                 sv +=1
 
+        robot.turn(-15*k)
         #Svängsekvens
-        robot.drive_time(190, 0, 1100)
+        robot.drive_time(200, 0, 1100)
         robot.turn(160)
 
         #Failsafe
-        if turn == "right":
-            k = -1
-        elif turn == "left":
-            k = 1
+        k=-1
         turnkonst = 0
         sv = 0
         lastcolor = color_sensor.color()
@@ -432,12 +450,12 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv = 0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 80:
-                robot.turn(80*k)
+            #wait(500)
+            if turnkonst == 84:
+                robot.turn(84*k)
                 k *= -1
                 turnkonst = 0
                 sv +=1
@@ -451,7 +469,7 @@ while True:
     elif color in POSSIBLE_COLORS:
         deposit +=1
         #Sekvens så att färgsensr åker förbi tejpen och inte läser av den igen
-        robot.drive_time(32, 0, 800)
+        robot.drive_time(40, 0, 800)
 
         #Failsafe
         if turn == "right":
@@ -465,12 +483,12 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv=0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 80:
-                robot.turn(80*k)
+            #wait(500)
+            if turnkonst == 84:
+                robot.turn(84*k)
                 k *= -1
                 turnkonst = 0
                 sv += 1
@@ -492,21 +510,21 @@ while True:
             if sv >=2:
                 robot.straight(20)
                 sv=0
-            turnkonst += 5
-            robot.turn(-5*k)
+            turnkonst += 6
+            robot.turn(-6*k)
             lastcolor = color_sensor.color()
-            wait(500)
-            if turnkonst == 80:
-                robot.turn(80*k)
+            #wait(500)
+            if turnkonst == 84:
+                robot.turn(84*k)
                 k *= -1
                 turnkonst = 0
                 sv += 1
     elif rsensor < Black:
-        turn_rate = ((Black-rsensor)*5 + 40)
+        turn_rate = ((Black-rsensor)*6 + 40)
         turn = "right"
         #print("höger")
     elif lsensor < Black:
-        turn_rate = -((Black-lsensor)*5 + 40)
+        turn_rate = -((Black-lsensor)*6 + 40)
         turn = "left"
         #print("vänster")
     else:
